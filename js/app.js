7899942eafc7c7bfa69bc6a568656a5a692165fb
mamarok2 <mamarok@gmail.com>
2016-06-14 13:52:18 +0200

    Added some JS functionality

--- js/app.js
+++ js/app.js
@@ -12,28 +12,82 @@ $(function() {
     var hamburgerMenu = $(".menu");
 
     hamburgerIcon.on("click", function() {
-
         hamburgerMenu.toggleClass("hidden");
+    });
+
+    //Hidden content showcase:
+
+    //Speciality-section
+
+    var iconDivs = $(".icon-div");
+
+    iconDivs.on("mouseenter ", function() {
+
+        $(this).find("div").toggleClass("content-shown");
+        $(this).toggleClass("content-bg");
 
     });
 
-//Speciality showcase
+    iconDivs.on("mouseleave", function() {
 
-var iconDivs = $(".icon-div");
+        $(this).find("div").toggleClass("content-shown");
+        $(this).toggleClass("content-bg");
 
-iconDivs.on("mouseenter",function(){
+    });
 
-$(this).find("div").toggleClass("content-shown");
-$(this).toggleClass("content-bg");
+    //Team-section
 
-});
+    var teamDivs = $(".team-div");
 
-iconDivs.on("mouseleave",function(){
+    teamDivs.on("mouseenter", function() {
+        $(this).find("p").toggleClass("invisible");
+        $(this).find("figure").toggleClass("invisible");
 
-  $(this).find("div").toggleClass("content-shown");
-  $(this).toggleClass("content-bg");
+    });
 
-});
+    teamDivs.on("mouseleave", function() {
+        $(this).find("p").toggleClass("invisible");
+        $(this).find("figure").toggleClass("invisible");
+    });
+
+    //FAQ-section
+
+    var faqDivs = $(".FAQ-question");
+
+    $(".answer").hide();
+    faqDivs.on("click", function() {
+        $(this).find(".answer").slideToggle("slow");
+    });
+
+    //Brand-section
+
+    var brandDivs = $(".brand-logo");
+
+    brandDivs.on("mouseenter ", function() {
+
+        $(this).find("p").toggleClass("content-shown");
+    });
+
+    brandDivs.on("mouseleave", function() {
+
+        $(this).find("p").toggleClass("content-shown");
+    });
+
+    // Mobile-desgin-section
+
+    var learnMoreBtn = $(".btn-holder").find("button");
+
+    learnMoreBtn.on("click ", function() {
+
+        if ($(window).width() >= 1183) {
+            $(".mobile-design-section").toggleClass("m-d-section-animate");
+            $(".btn-holder").find("div").toggleClass("m-d-content-animate");
+        } else {
+            $(".btn-holder").find("div").toggleClass("m-d-hidden");
+        }
+    });
+
+    // -------------------------------------------------------------------------------------------------------------
 
     //Sticky menu
 
@@ -53,13 +107,15 @@ iconDivs.on("mouseleave",function(){
     });
 
     // Scroll into view
-    var menuBtns = $(".menu-bar").find("a");
 
-    menuBtns.on("click", function(event) {
+    var menuBtns = $(".menu-bar").find("a");
+    var contactBtn = $(".btn-holder").find("a");
+    var stickyMenuHeight = stickyMenu.outerHeight();
+
+    menuBtns.add(contactBtn).on("click", function(event) {
 
         var scrollToId = $(this).attr('href');
         var sectionOffset = $(scrollToId).offset().top;
-        var stickyMenuHeight = stickyMenu.outerHeight();
         event.preventDefault();
 
         if ($(window).width() >= 751) {
@@ -76,6 +132,7 @@ iconDivs.on("mouseleave",function(){
     $(window).resize(function() {
         htmlHeight = htmlDocument.outerHeight(true);
         $(window).scrollTop(position * htmlHeight);
+        console.log($(window).width());
 
     });
 
